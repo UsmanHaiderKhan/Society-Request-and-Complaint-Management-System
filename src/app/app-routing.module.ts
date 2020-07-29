eef9bfe52448cfcb730d8094af98927e3e8d637c
UsmanHaiderKhan4 <usmanhaiderkhan4@gmail.com>
2020-07-29 22:56:40 +0500

    update added 🛣 routing of application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,49 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ComplainComponent } from './components/home/complain/complain.component';
+import { RequestComponent } from './components/home/request/request.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { ReciveRequestComponent } from './components/admin/recive-request/recive-request.component';
+import { ReciveComplainComponent } from './components/admin/recive-complain/recive-complain.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'complain',
+    component: ComplainComponent,
+  },
+  {
+    path: 'request',
+    component: RequestComponent,
+  },
+  {
+    path: 'admin',
+    component: AdminComponent,
+  },
+  {
+    path: 'reciverequest',
+    component: ReciveRequestComponent,
+  },
+  {
+    path: 'recivecomplain',
+    component: ReciveComplainComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,65 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+// 1. Import the libs you need
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+// import { environment } from '../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { RequestComponent } from './components/home/request/request.component';
+import { ComplainComponent } from './components/home/complain/complain.component';
+import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { ReciveRequestComponent } from './components/admin/recive-request/recive-request.component';
+import { ReciveComplainComponent } from './components/admin/recive-complain/recive-complain.component';
+import { OperationService } from './components/shared/services/operation.service';
+import { ComplainService } from './components/shared/services/complain.service';
+import { from } from 'rxjs';
+import { ModalComponent } from './components/admin/modal/modal.component';
 
+// Database Connectino Method
+var firebaseConfig = {
+  apiKey: 'AIzaSyDs8XCIrJ1Emqg9V5Kk9ViYpdBqbrc7AcQ',
+  authDomain: 'complaintmanagementsyste-e927a.firebaseapp.com',
+  databaseURL: 'https://complaintmanagementsyste-e927a.firebaseio.com',
+  projectId: 'complaintmanagementsyste-e927a',
+  storageBucket: 'complaintmanagementsyste-e927a.appspot.com',
+  messagingSenderId: '1017477332778',
+  appId: '1:1017477332778:web:5cd7e9084dcda89d4fa853',
+  measurementId: 'G-86FFLGM5RC',
+};
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    RequestComponent,
+    ComplainComponent,
+    SidebarComponent,
+    AdminComponent,
+    ReciveRequestComponent,
+    ReciveComplainComponent,
+    ModalComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot(),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  schemas: [NO_ERRORS_SCHEMA],
+  providers: [OperationService, ComplainService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
