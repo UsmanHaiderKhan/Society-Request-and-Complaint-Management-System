af132e34fec3abfb55946ed1415347a1d4774dd8
UsmanHaiderKhan4 <usmanhaiderkhan4@gmail.com>
2020-08-02 23:40:11 +0500

    update : firebase database path updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
 // 1. Import the libs you need
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireStorageModule } from '@angular/fire/storage';
-// import { environment } from '../environments/environment';
-
+import { environment } from '../environments/environment';
+// Components imports
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-import { HeaderComponent } from './components/shared/header/header.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
 import { RequestComponent } from './components/home/request/request.component';
 import { ComplainComponent } from './components/home/complain/complain.component';
@@ -20,46 +18,60 @@ import { ReciveRequestComponent } from './components/admin/recive-request/recive
 import { ReciveComplainComponent } from './components/admin/recive-complain/recive-complain.component';
 import { OperationService } from './components/shared/services/operation.service';
 import { ComplainService } from './components/shared/services/complain.service';
+import { NotificationService } from './components/shared/services/notification.service';
 import { from } from 'rxjs';
-import { ModalComponent } from './components/admin/modal/modal.component';
 import { SidebarComponent } from './components/admin/sidebar/sidebar.component';
+import { DashboardComponent } from './components/admin/dashboard/dashboard.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCardModule, MatCard } from '@angular/material/card';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { AdminheaderComponent } from './components/admin/adminheader/adminheader.component';
+import { HeaderComponent } from './components/home/header/header.component';
+import { ModalComponent } from './components/admin/modal/modal.component';
 
-// Database Connectino Method
-var firebaseConfig = {
-  apiKey: 'AIzaSyDs8XCIrJ1Emqg9V5Kk9ViYpdBqbrc7AcQ',
-  authDomain: 'complaintmanagementsyste-e927a.firebaseapp.com',
-  databaseURL: 'https://complaintmanagementsyste-e927a.firebaseio.com',
-  projectId: 'complaintmanagementsyste-e927a',
-  storageBucket: 'complaintmanagementsyste-e927a.appspot.com',
-  messagingSenderId: '1017477332778',
-  appId: '1:1017477332778:web:5cd7e9084dcda89d4fa853',
-  measurementId: 'G-86FFLGM5RC',
-};
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent,
     FooterComponent,
     RequestComponent,
     ComplainComponent,
     AdminComponent,
     ReciveRequestComponent,
     ReciveComplainComponent,
-    ModalComponent,
     SidebarComponent,
+    DashboardComponent,
+    AdminheaderComponent,
+    HeaderComponent,
+    ModalComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
     AngularFireStorageModule,
     AppRoutingModule,
-    MDBBootstrapModule.forRoot(),
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatCardModule,
+    MatBadgeModule,
+    MatDialogModule,
+    MatSnackBarModule,
+    // forRoot(),
+    BrowserAnimationsModule,
   ],
   schemas: [NO_ERRORS_SCHEMA],
-  providers: [OperationService, ComplainService],
+  providers: [OperationService, ComplainService, NotificationService],
   bootstrap: [AppComponent],
+  entryComponents: [ReciveComplainComponent],
 })
 export class AppModule {}
