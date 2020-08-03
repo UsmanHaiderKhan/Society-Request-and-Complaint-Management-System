4a6f4ded12912eb26d679eb099704080294aeaa1
UsmanHaiderKhan4 <usmanhaiderkhan4@gmail.com>
2020-08-03 23:43:46 +0500

    update added new  methods to perform some operations

--- src/app/components/shared/services/operation.service.ts
+++ src/app/components/shared/services/operation.service.ts
@@ -1,25 +1,72 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
 export class OperationService {
-  constructor(public fireService: AngularFirestore) {}
+  requetList: AngularFireList<any>;
+
+  constructor(public firebase: AngularFireDatabase) {}
+
+  form: FormGroup = new FormGroup({
+    $key: new FormControl(null),
+    fullname: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.email),
+    address: new FormControl(''),
+    mobile: new FormControl('', [
+      Validators.required,
+      Validators.minLength(11),
+    ]),
+    requestType: new FormControl(1),
+    imageUrl: new FormControl(''),
+    requestDetail: new FormControl(''),
+  });
+  // Initialize
+  onInitialLizeFormGroup() {
+    this.form.setValue({
+      $key: null,
+      fullname: '',
+      email: '',
+      address: '',
+      mobile: '',
+      requestType: '1',
+      imageUrl: '',
+      requestDetail: '',
+    });
+  }
 
   //Add Method
   submitRequest(Request) {
-    return this.fireService.collection('request').add(Request);
+    this.requetList.push({
+      fullname: Request.fullname,
+      email: Request.email,
+      address: Request.address,
+      mobile: Request.mobile,
+      requestType: Request.gender,
+      imageUrl: Request.imageUrl,
+      requestDetail: Request.requestDetail,
+    });
   }
   //Get Method
   getAllRequests(): Observable<any> {
-    return this.fireService.collection('request').snapshotChanges();
+    this.requetList = this.firebase.list('request');
+    return this.requetList.snapshotChanges();
   } //Update Method
-  updateRequestData(recordRequestId, requests) {
-    return this.fireService.doc('request/' + recordRequestId).update(requests);
+  updateRequest(request) {
+    this.requetList.update(request.$key, {
+      fullname: request.fullname,
+      email: request.email,
+      address: request.address,
+      mobile: request.mobile,
+      requestType: request.requestType,
+      imageUrl: request.imageUrl,
+      requestDetail: request.requestDetail,
+    });
   }
   //Delete Method
-  deleteRequestData(requestId) {
-    return this.fireService.doc('request/' + requestId).delete();
+  deleteRequestData($key: string) {
+    this.requetList.remove($key);
   }
 }
