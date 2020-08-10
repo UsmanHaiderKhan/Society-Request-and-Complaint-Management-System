b3100e526ae2b59cfa020805fbb3432da799d802
UsmanHaiderKhan4 <usmanhaiderkhan4@gmail.com>
2020-08-10 23:50:58 +0500

    update added modal components

--- src/app/components/admin/modal/modal.component.ts
+++ src/app/components/admin/modal/modal.component.ts
@@ -1,17 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { OperationService } from '../../shared/services/operation.service';
-import { from } from 'rxjs';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.scss'],
+  styleUrls: ['./modal.component.scss']
 })
 export class ModalComponent implements OnInit {
-  constructor(public operationService: OperationService) {}
 
-  ngOnInit(): void {}
-  public deleteRequest(requestId) {
-    this.operationService.deleteRequestData(requestId);
+  constructor() { }
+
+  ngOnInit(): void {
   }
+
 }
