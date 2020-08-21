0055b37e3c2ac2696915792d41addad6393b673c
UsmanHaiderKhan4 <usmanhaiderkhan4@gmail.com>
2020-08-21 23:31:00 +0500

    update the Request Component

--- src/app/components/home/request/request.component.ts
+++ src/app/components/home/request/request.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { OperationService } from '../../shared/services/operation.service';
 import { AngularFireStorage } from '@angular/fire/storage';
-import { NotificationService } from '../../shared/services/notification.service';
 import { finalize } from 'rxjs/operators';
+import { NotificationService } from '../../shared/services/notification.service';
 
 @Component({
   selector: 'app-request',
@@ -12,7 +12,7 @@ import { finalize } from 'rxjs/operators';
 export class RequestComponent implements OnInit {
   optionsSelect: Array<any>;
   selectedImage: any = null;
-  imageUrl = '../../../../assets/images/avatar.png';
+  imgSrc = '../../../../assets/images/avatar.png';
   constructor(
     public operationService: OperationService,
     public notificationService: NotificationService,
@@ -21,16 +21,34 @@ export class RequestComponent implements OnInit {
 
   ngOnInit() {
     this.optionsSelect = [
-      { value: 'Normal', label: 'Normal' },
-      { value: 'Severe', label: 'Severe' },
-      { value: 'Urgent', label: 'Urgent' },
+      { value: '1', label: 'Normal' },
+      { value: '2', label: 'Severe' },
+      { value: '3', label: 'Urgent' },
     ];
   }
-  submitRequest() {
+
+  submitRequest(formValue) {
     if (this.operationService.form.valid) {
-      if (!this.operationService.form.get('$key').value)
-        this.operationService.submitRequest(this.operationService.form.value);
-      else {
+      if (!this.operationService.form.get('$key').value) {
+        // this.operationService.submitRequest(this.operationService.form.value);
+        var filePath = `images/${this.selectedImage.name
+          .split('.')
+          .slice(0, -1)
+          .join('.')}_${new Date().getTime()}`;
+        const fileRef = this.storage.ref(filePath);
+        this.storage
+          .upload(filePath, this.selectedImage)
+          .snapshotChanges()
+          .pipe(
+            finalize(() => {
+              fileRef.getDownloadURL().subscribe((url) => {
+                formValue['imageUrl'] = url;
+                this.operationService.submitRequest(formValue);
+              });
+            })
+          )
+          .subscribe();
+      } else {
         this.operationService.updateRequest(this.operationService.form.value);
       }
       this.operationService.form.reset();
@@ -39,15 +57,21 @@ export class RequestComponent implements OnInit {
       this.onClose();
     }
   }
-  onClose() {}
+  onClose() {
+    this.operationService.form.reset();
+    this.operationService.onInitialLizeFormGroup();
+  }
+  get formControls() {
+    return this.operationService.form['controls'];
+  }
   showImagePreview(event: any) {
     if (event.target.files && event.target.files[0]) {
       const reader = new FileReader();
-      reader.onload = (e: any) => (this.imageUrl = e.target.result);
+      reader.onload = (e: any) => (this.imgSrc = e.target.result);
       reader.readAsDataURL(event.target.files[0]);
       this.selectedImage = event.target.files[0];
     } else {
-      this.imageUrl = '../../../../assets/images/avatar.png';
+      this.imgSrc = '../../../../assets/images/avatar.png';
       this.selectedImage = null;
     }
   }
