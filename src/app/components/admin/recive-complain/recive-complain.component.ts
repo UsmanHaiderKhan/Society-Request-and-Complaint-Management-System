36d9cf9a24f97af6c4ff7fa76bd6fbff5d1033d2
UsmanHaiderKhan4 <usmanhaiderkhan4@gmail.com>
2020-08-24 00:11:17 +0500

    Update added Table for Showing Complains data

--- src/app/components/admin/recive-complain/recive-complain.component.ts
+++ src/app/components/admin/recive-complain/recive-complain.component.ts
@@ -1,6 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ComplainService } from '../../shared/services/complain.service';
+import { NotificationService } from '../../shared/services/notification.service';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { finalize } from 'rxjs/operators';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
+import { ComplainComponent } from '../../home/complain/complain.component';
 
 @Component({
   selector: 'app-recive-complain',
@@ -10,9 +16,26 @@ import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 export class ReciveComplainComponent implements OnInit {
   complain: any;
   optionsSelect: Array<any>;
+
+  listData: MatTableDataSource<any>;
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  searchKey: string;
+  displayedColumns: string[] = [
+    'imageUrl',
+    'fullname',
+    'email',
+    'address',
+    'mobile',
+    'complainType',
+    'complainDetail',
+    'actions',
+  ];
+
   constructor(
     public complainService: ComplainService,
-    public dialog: MatDialog
+    public dialog: MatDialog,
+    public notificationService: NotificationService
   ) {}
 
   ngOnInit(): void {
@@ -32,19 +55,20 @@ export class ReciveComplainComponent implements OnInit {
     this.dialog.open(ReciveComplainComponent, dialogConfig);
   }
   public loadData() {
-    this.complainService.getAllComplains().subscribe((data) => {
-      this.complain = data.map((e) => {
-        return {
-          id: e.payload.doc.id,
-          isedit: false,
-          fullname: e.payload.doc.data()['fullname'],
-          email: e.payload.doc.data()['email'],
-          address: e.payload.doc.data()['address'],
-          phonenumber: e.payload.doc.data()['phonenumber'],
-          complainType: e.payload.doc.data()['complainType'],
-          complainDetails: e.payload.doc.data()['complainDetails'],
-        };
+    this.complainService.getAllComplains().subscribe((list) => {
+      let array = list.map((item) => {
+        return { $key: item.key, ...item.payload.val() };
       });
+      this.listData = new MatTableDataSource(array);
+      this.listData.sort = this.sort;
+      this.listData.paginator = this.paginator;
+      this.listData.filterPredicate = (data, filter) => {
+        return this.displayedColumns.some((ele) => {
+          return (
+            ele != 'actions' && data[ele].toLowerCase().indexOf(filter) != -1
+          );
+        });
+      };
     });
   }
   public editComplain(complain) {
@@ -67,6 +91,31 @@ export class ReciveComplainComponent implements OnInit {
     complain['complainType'] = recordComplain.editcomplainType;
     complain['complainDetails'] = recordComplain.editcomplainDetails;
     // here calling service
-    this.complainService.updateComplainData(recordComplain.id, complain);
+    this.complainService.updateComplain(complain);
+  }
+  public deleteRequest($key: string) {
+    this.complainService.deleteComplainData($key);
+  }
+
+  onSearchClear() {
+    this.searchKey = '';
+    this.applyFilter();
+  }
+  applyFilter() {
+    this.listData.filter = this.searchKey.trim().toLowerCase();
+  }
+
+  onEdit(row) {
+    this.complainService.updateComplain(row);
+    let config = new MatDialogConfig();
+
+    (config.disableClose = true), (config.width = '60%');
+    this.dialog.open(ComplainComponent, config);
+  }
+  onDelete($key) {
+    if (confirm('Are you Sure you Want to Delete Record?')) {
+      this.complainService.deleteComplainData($key);
+      this.notificationService.danger('Deleted SuccessFully');
+    }
   }
 }
